7dae64b51b9a2b02dd280acb4b41c1cd37781587
Jonathan Lloyd <jonathan@thisisjonathan.com>
2018-12-08 19:51:12 +0000

    Day 05: Use stack rather than linked list

--- 05/part1.py
+++ 05/part1.py
@@ -1,62 +1,42 @@
-from dataclasses import dataclass
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.items:
+            return self.items.pop()
+        else:
+            return None
+
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+        else:
+            return None
 
 
-def is_upper(string):
-    return string == string.upper()
+def should_react(a, b):
+    return a.lower() == b.lower() and a != b
 
-def is_lower(string):
-    return string == string.lower()
+def react(polymer):
+    stack = Stack()
+    for unit in polymer:
+        head = stack.peek()
+        if head and should_react(unit, head):
+            stack.pop()
+        else:
+            stack.push(unit)
 
-
-@dataclass
-class Element:
-    value: str
-    left: str = None
-    right: str = None
-
-    def __repr__(self):
-        return f"Element(value='{self.value}', left={self.left is not None}, right={self.right is not None})"
+    return ''.join(stack.items)
 
 
 if __name__ == '__main__':
     with open('input', 'r') as f:
         polymer = f.readline()
 
-    polymer = polymer[:-1] # Remove trailing newline
-
-    head = Element(value='')
-    tail = Element(value='')
-    prev = head
-    for char in polymer:
-        elem = Element(value=char)
-        prev.right = elem
-        elem.left = prev
-        prev = elem
-    elem.right = tail
-
-    current_elem = head.right
-    while current_elem is not tail:
-        prev_elem = current_elem.left
-        next_elem = current_elem.right
-        should_react = (
-            current_elem.value.lower() == next_elem.value.lower()
-            and (
-                (is_upper(current_elem.value) and is_lower(next_elem.value))
-                or (is_lower(current_elem.value) and is_upper(next_elem.value))
-            )
-        )
-        if should_react:
-            next_next_elem = next_elem.right
-            prev_elem.right = next_next_elem
-            next_next_elem.left = prev_elem
-            current_elem = prev_elem
-        else:
-            current_elem = next_elem
-
-    result = ''
-    current_elem = head.right
-    while current_elem is not tail:
-        result += current_elem.value
-        current_elem = current_elem.right
-
-    print(len(result))
+    polymer = polymer.strip() # Remove trailing newline
+    reduced_polymer = react(polymer)
+    print(len(reduced_polymer))
--- 05/part2.py
+++ 05/part2.py
@@ -1,74 +1,57 @@
-from dataclasses import dataclass
 import string
 
+class Stack:
+    def __init__(self):
+        self.items = []
 
-def is_upper(string):
-    return string == string.upper()
+    def push(self, item):
+        self.items.append(item)
 
-def is_lower(string):
-    return string == string.lower()
+    def pop(self):
+        if self.items:
+            return self.items.pop()
+        else:
+            return None
+
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+        else:
+            return None
 
 
-@dataclass
-class Element:
-    value: str
-    left: str = None
-    right: str = None
+def should_react(a, b):
+    return a.lower() == b.lower() and a != b
 
-    def __repr__(self):
-        return f"Element(value='{self.value}', left={self.left is not None}, right={self.right is not None})"
+def react(polymer):
+    stack = Stack()
+    for unit in polymer:
+        head = stack.peek()
+        if head and should_react(unit, head):
+            stack.pop()
+        else:
+            stack.push(unit)
+
+    return ''.join(stack.items)
+
+def strip_unit(polymer, target_unit):
+    return ''.join(
+        unit
+        for unit in polymer
+        if unit.lower() != target_unit.lower()
+    )
 
 
 if __name__ == '__main__':
     with open('input', 'r') as f:
-        base_polymer = f.readline()
-    base_polymer = base_polymer[:-1] # Remove trailing newline
+        polymer = f.readline()
+
+    polymer = polymer.strip() # Remove trailing newline
 
     lengths = []
-    for target_unit in string.ascii_lowercase:
-        polymer = ''.join(
-            unit
-            for unit in base_polymer
-            if unit.lower() != target_unit.lower()
-        )
+    for unit in string.ascii_lowercase:
+        stripped_polymer = strip_unit(polymer, unit)
+        reduced_polymer = react(stripped_polymer)
+        lengths.append(len(reduced_polymer))
 
-        head = Element(value='')
-        tail = Element(value='')
-        prev = head
-        for char in polymer:
-            elem = Element(value=char)
-            prev.right = elem
-            elem.left = prev
-            prev = elem
-        elem.right = tail
-
-        current_elem = head.right
-        while current_elem is not tail:
-            prev_elem = current_elem.left
-            next_elem = current_elem.right
-            should_react = (
-                current_elem.value.lower() == next_elem.value.lower()
-                and (
-                    (is_upper(current_elem.value) and is_lower(next_elem.value))
-                    or (is_lower(current_elem.value) and is_upper(next_elem.value))
-                )
-            )
-            if should_react:
-                next_next_elem = next_elem.right
-                prev_elem.right = next_next_elem
-                next_next_elem.left = prev_elem
-                current_elem = prev_elem
-            else:
-                current_elem = next_elem
-
-        result = ''
-        current_elem = head.right
-        while current_elem is not tail:
-            result += current_elem.value
-            current_elem = current_elem.right
-
-        lengths.append((target_unit, len(result)))
-
-    shortest_polymer = min(lengths, key=lambda x: x[1])
-    _, shortest_polymer_length = shortest_polymer
-    print(shortest_polymer_length)
+    print(min(lengths))
